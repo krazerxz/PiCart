87ad117df6f5445da91fb9305cef2ca6a1814914
Chris Pomfret <chris.pomfret@bt.com>
2015-10-01 13:35:01 +0100

    remove silly specs

--- spec/routing/products_routing_spec.rb
+++ spec/routing/products_routing_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe ProductsController, type: :routing do
   describe "routing" do
-
     it "routes to #index" do
       expect(:get => "/products").to route_to("products#index")
     end
